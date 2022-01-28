72a5d8db60507bd97b55e862c08031cf6ad07a38
manjugoutham <gouthamy219@gmail.com>
2022-01-28 07:51:16 +0530

    Create address book contact

---
+++ address.js
@@ -0,0 +1,20 @@
+/**
+ * Ability to create a Address Book Contact with first and last names,
+ * address, city, state, zip, phone number and email.
+ * 
+ * @param {*} firstName 
+ * @param {*} lastName 
+ * @param {*} email 
+ * @param {*} number 
+ * @param {*} zipcode 
+ * @param {*} address 
+ * @param {*} city 
+ * @param {*} state 
+ * @returns 
+ */
+ function addPerson(firstName, lastName, email, number, zipcode, address, city, state) {
+       console.log(firstName + "\n" + lastName + "\n" + email + "\n" + number + "\n" + zipcode + "\n" + address + "\n" + city + "\n" + state);
+   // return [firstName, lastName, email, number, zipcode, address, city, state];
+}
+
+addPerson("Goutham", "Raj", "gouthamy21@gmail.com", "8105457612", 560077, "near RTNagar", "Bengaluru", "Karnataka")
